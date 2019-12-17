bbd1294b1ba70c76adedb1e303ea1c9201d96578
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-12-17 15:46:57 -0500

    [#master] fixing test file for new constructor
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- tic_tac_toe_test.py
+++ tic_tac_toe_test.py
@@ -7,67 +7,67 @@ class TicTacToeStateTest(TestCase):
 				['X', 'O', 'O'],
 				['O', 'X', 'X']]
 
-		self.assertTrue(TicTacToeState('X', 'O', grid).is_terminal())
+		self.assertTrue(TicTacToeState('X', 'O', grid, 'X', 'O').is_terminal())
 
 	def test_is_terminal_top_row_win(self):
 		grid = [['X', 'X', 'X'],
 				[' ', 'O', 'O'],
 				[' ', ' ', ' ']]
 
-		self.assertTrue(TicTacToeState('X', 'O', grid).is_terminal())
+		self.assertTrue(TicTacToeState('X', 'O', grid, 'X', 'O').is_terminal())
 
 	def test_is_terminal_middle_row_win(self):
 		grid = [['X', ' ', 'X'],
 				['O', 'O', 'O'],
 				[' ', 'X', 'O']]
 
-		self.assertTrue(TicTacToeState('X', 'O', grid).is_terminal())
+		self.assertTrue(TicTacToeState('X', 'O', grid, 'X', 'O').is_terminal())
 
 	def test_is_terminal_bottom_row_win(self):
 		grid = [[' ', 'O', 'X'],
 				[' ', 'O', 'O'],
 				['X', 'X', 'X']]
 
-		self.assertTrue(TicTacToeState('X', 'O', grid).is_terminal())
+		self.assertTrue(TicTacToeState('X', 'O', grid, 'X', 'O').is_terminal())
 
 	def test_is_terminal_left_col_win(self):
 		grid = [['X', 'O', ' '],
 				['X', ' ', 'O'],
 				['X', 'O', 'X']]
 
-		self.assertTrue(TicTacToeState('X', 'O', grid).is_terminal())
+		self.assertTrue(TicTacToeState('X', 'O', grid, 'X', 'O').is_terminal())
 
 	def test_is_terminal_middle_col_win(self):
 		grid = [['X', 'O', ' '],
 				[' ', 'O', ' '],
 				['X', 'O', 'X']]
 
-		self.assertTrue(TicTacToeState('X', 'O', grid).is_terminal())
+		self.assertTrue(TicTacToeState('X', 'O', grid, 'X', 'O').is_terminal())
 
 	def test_is_terminal_right_col_win(self):
 		grid = [['X', ' ', 'O'],
 				[' ', 'X', 'O'],
 				['X', ' ', 'O']]
 
-		self.assertTrue(TicTacToeState('X', 'O', grid).is_terminal())
+		self.assertTrue(TicTacToeState('X', 'O', grid, 'X', 'O').is_terminal())
 
 	def test_is_terminal_right_diagonal_win(self):
 		grid = [['X', ' ', 'O'],
 				['O', 'X', 'O'],
 				['X', ' ', 'X']]
 
-		self.assertTrue(TicTacToeState('X', 'O', grid).is_terminal())
+		self.assertTrue(TicTacToeState('X', 'O', grid, 'X', 'O').is_terminal())
 
 	def test_is_terminal_left_diagonal_win(self):
 		grid = [['O', ' ', 'X'],
 				['O', 'X', 'O'],
 				['X', ' ', 'X']]
 
-		self.assertTrue(TicTacToeState('X', 'O', grid).is_terminal())
+		self.assertTrue(TicTacToeState('X', 'O', grid, 'X', 'O').is_terminal())
 
 	def test_is_terminal_no_win(self):
 		grid = [['O', ' ', 'X'],
 				['O', ' ', 'O'],
 				['X', ' ', 'X']]
 
-		self.assertFalse(TicTacToeState('X', 'O', grid).is_terminal())
+		self.assertFalse(TicTacToeState('X', 'O', grid, 'X', 'O').is_terminal())
